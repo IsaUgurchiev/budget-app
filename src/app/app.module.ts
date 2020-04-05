7eba9219de672b17b4c19dba975b544d028e68a7
Isa Ugurchiev <iugurchiev@iponweb.net>
2020-04-06 00:04:30 +0300

    Added items

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { AddItemFormComponent } from './add-item-form/add-item-form.component';
+import { BudgetItemListComponent } from './budget-item-list/budget-item-list.component';
+import { BugetItemCardComponent } from './budget-item-list/buget-item-card/buget-item-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    AddItemFormComponent,
+    BudgetItemListComponent,
+    BugetItemCardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/budget-item-list/buget-item-card/buget-item-card.component.ts
@@ -0,0 +1,17 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-buget-item-card',
+  templateUrl: './buget-item-card.component.html',
+  styleUrls: ['./buget-item-card.component.scss']
+})
+export class BugetItemCardComponent implements OnInit {
+
+  @Input() isIncome = true;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
